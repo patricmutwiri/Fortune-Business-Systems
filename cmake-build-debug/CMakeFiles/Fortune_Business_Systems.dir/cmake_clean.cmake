791d50b36389a13cb2d469c2e0e3cdd347e1fec0
Patrick Mutwiri <patwiri@gmail.com>
2020-03-26 22:54:18 +0300

    Fortune Business Systems

---
+++ cmake-build-debug/CMakeFiles/Fortune_Business_Systems.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fortune_Business_Systems.dir/main.cpp.o"
+  "Fortune_Business_Systems"
+  "Fortune_Business_Systems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fortune_Business_Systems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Fortune_Business_Systems.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Fortune_Business_Systems.
+# This may be replaced when dependencies are built.
