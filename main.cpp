d907dee7acb4f00eb06960f887ee675d8029d0a9
Patrick Mutwiri <patwiri@gmail.com>
2020-03-26 23:11:43 +0300

    c++ /constructor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
+/* 
+    @author Patrick Mutwiri <patwiri@gmail.com>
+    @patric_mutwiri
+    Created by patrick on 3/26/20.    
+*/
+//
+
+#include "main.h"
 #include <iostream>
+using namespace std;
+class employee {
+public:
+    employee();
+    string empid,fname,sname,surname,dob;
+    char gender;
+    int dd,mm,yyyy;
+    float basicSalary;
+
+    // show employee
+    static void show_employee(){
+
+    }
+};
+employee::employee(){
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    employee emp_obj;
+    cout << " :: FORTUNE BUSINESS SYSTEMS :: " << std::endl;
+    cout<<" "<<endl;
+    cout<<"ADD EMPLOYEE DETAILS\n";
+    cout<<"---------------------------\n";
+    employee::show_employee();
+
+    cout<<" \n";
+    cout<<"SHOW EMPLOYEE DETAILS\n";
+    cout<<"---------------------------\n";
+
+    cout<<" :: FBS :: "<<endl;
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,17 @@
+/* 
+    @author Patrick Mutwiri <patwiri@gmail.com>
+    @patric_mutwiri
+    Created by patrick on 3/26/20.    
+*/
+//
+
+#ifndef FORTUNE_BUSINESS_SYSTEMS_MAIN_H
+#define FORTUNE_BUSINESS_SYSTEMS_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //FORTUNE_BUSINESS_SYSTEMS_MAIN_H
